e0fe9c2a6ca85ffe6a97d2d5f603f07af330345c
Kristin Agcaoili <kagcaoil@ucsd.edu>
2018-04-30 18:15:58 -0700

    User can draw directly onto whiteboard now

--- Assets/Marker.cs
+++ Assets/Marker.cs
@@ -8,24 +8,32 @@ public class Marker : MonoBehaviour {
     public float width;
     public float offset;
 
+    private Ray ray;
+
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButton(0))
+        Vector3 mousePos = Input.mousePosition;
+        ray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
         {
-            Vector3 mousePos = Input.mousePosition;
-            Debug.Log("Clicked mouse at screen point: " + mousePos);
-            Vector3 worldSpace = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10.0f));
-            LineRenderer currScribble = (LineRenderer) Instantiate(scribble, new Vector3(worldSpace.x, worldSpace.y, 0.0f), Quaternion.identity);
-            currScribble.startWidth = width;
-            currScribble.endWidth = width;
+            if (hit.collider.gameObject.GetComponent<Whiteboard>())
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    //Vector3 worldSpace = Camera.main.ScreenToWorldPoint(new Vector3(hit.point.x, hit.point.y, 10.0f));
+                    LineRenderer currScribble = (LineRenderer)Instantiate(scribble, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity);
+                    currScribble.startWidth = width;
+                    currScribble.endWidth = width;
 
-            currScribble.SetPosition(0, worldSpace);
-            currScribble.SetPosition(1, new Vector3(worldSpace.x+offset, worldSpace.y+offset, worldSpace.z));
+                    currScribble.SetPosition(0, hit.point);
+                    currScribble.SetPosition(1, new Vector3(hit.point.x + offset, hit.point.y + offset, hit.point.z));
+                }
+            }
         }
 	}
 }
