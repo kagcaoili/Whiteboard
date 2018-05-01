e36016a1649a78a959e5b31543f58dce8b21d002
Kristin Agcaoili <kagcaoil@ucsd.edu>
2018-04-30 18:05:44 -0700

    User can draw using line renderer

--- Assets/Marker.cs
+++ Assets/Marker.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Marker : MonoBehaviour {
 
-    public GameObject cube;
+    public LineRenderer scribble;
+    public float width;
+    public float offset;
 
 	// Use this for initialization
 	void Start () {
@@ -13,13 +15,17 @@ public class Marker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0))
+		if (Input.GetMouseButton(0))
         {
-            
             Vector3 mousePos = Input.mousePosition;
             Debug.Log("Clicked mouse at screen point: " + mousePos);
             Vector3 worldSpace = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10.0f));
-            Instantiate(cube, new Vector3(worldSpace.x, worldSpace.y, 0.0f), Quaternion.identity);
+            LineRenderer currScribble = (LineRenderer) Instantiate(scribble, new Vector3(worldSpace.x, worldSpace.y, 0.0f), Quaternion.identity);
+            currScribble.startWidth = width;
+            currScribble.endWidth = width;
+
+            currScribble.SetPosition(0, worldSpace);
+            currScribble.SetPosition(1, new Vector3(worldSpace.x+offset, worldSpace.y+offset, worldSpace.z));
         }
 	}
 }
