183f80561f952d644469a5720073da17128b4e5d
Kristin Agcaoili <kagcaoil@ucsd.edu>
2018-05-07 17:20:46 -0700

    Made the marker more efficient

--- Assets/Marker.cs
+++ Assets/Marker.cs
@@ -9,10 +9,27 @@ public class Marker : MonoBehaviour {
     public float offset;
 
     private Ray ray;
+    private Vector3 lastPoint;
+
+    public float detectingRadius;
 
 	// Use this for initialization
 	void Start () {
 	}
+
+    private bool isMoving(Vector3 lastPosition, Vector3 currentPos)
+    {
+        if (currentPos.x < (lastPosition.x - detectingRadius) && currentPos.x > (lastPosition.x + detectingRadius))
+        {
+            if (currentPos.y < (lastPosition.y - detectingRadius) && currentPos.y > (lastPosition.y + detectingRadius))
+            {
+                Debug.Log("Moving from " + lastPoint + " to " + currentPos);
+                return true;
+            }
+        }
+
+        return false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,15 +40,33 @@ public class Marker : MonoBehaviour {
         {
             if (hit.collider.gameObject.GetComponent<Whiteboard>())
             {
-                if (Input.GetMouseButton(0))
+                if (Input.GetMouseButtonDown(0))
                 {
-                    //Vector3 worldSpace = Camera.main.ScreenToWorldPoint(new Vector3(hit.point.x, hit.point.y, 10.0f));
                     LineRenderer currScribble = (LineRenderer)Instantiate(scribble, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity);
                     currScribble.startWidth = width;
                     currScribble.endWidth = width;
 
                     currScribble.SetPosition(0, hit.point);
-                    currScribble.SetPosition(1, new Vector3(hit.point.x + offset, hit.point.y + offset, hit.point.z));
+
+                    lastPoint = new Vector3(hit.point.x + offset, hit.point.y + offset, hit.point.z);
+
+                    currScribble.SetPosition(1, lastPoint);
+
+                } else if (Input.GetMouseButton(0))
+                {
+                    Debug.Log("Last Point: " + lastPoint);
+                    Debug.Log("Currenet mosue position: " + hit.point);
+                    if (isMoving(lastPoint, hit.point))
+                    {
+                        LineRenderer currScribble = (LineRenderer)Instantiate(scribble, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity);
+                        currScribble.startWidth = width;
+                        currScribble.endWidth = width;
+
+                        currScribble.SetPosition(0, lastPoint);
+                        currScribble.SetPosition(1, hit.point);
+
+                        lastPoint = hit.point;
+                    }
                 }
             }
         }
