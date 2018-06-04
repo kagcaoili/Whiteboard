4545a7590213e9bcb953bde22a23b3b1ef3f266b
Connor Smith <c3smith@ucsd.edu>
2018-06-03 21:32:43 -0700

    Added lots of different modes

---
+++ Assets/Scripts/EnableObjectButton.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnableObjectButton : MonoBehaviour {
+
+    private static EnableObjectButton activeButton;
+    public GameObject objectToEnable;
+    public bool mutuallyExclusive = true;
+
+    public static Color inactiveColor = Color.white;
+    public static Color activeColor = Color.green;
+
+    private bool firstFrame = true;
+
+    public void Start()
+    {
+
+        SetButtonColor(inactiveColor);
+        SetEnabled(false);
+   
+    }
+
+    public void Update()
+    {
+
+
+    }
+
+    public void ToggleObject()
+    {
+        if (objectToEnable != null)
+        {
+
+            if (!objectToEnable.activeSelf && activeButton != this)
+            {
+
+                if (activeButton != null && mutuallyExclusive)
+                {
+
+                    activeButton.SetEnabled(false);
+                    activeButton = this;
+                }
+            }
+            else if (objectToEnable.activeSelf && mutuallyExclusive)
+            {
+                activeButton = null;
+            }
+
+            SetEnabled(!objectToEnable.activeSelf);
+        }
+    }
+
+    public void SetEnabled(bool enabled)
+    {
+        if (objectToEnable != null)
+        {
+            objectToEnable.SetActive(enabled);
+            
+            if (enabled)
+            {
+                SetButtonColor(activeColor);
+
+                if (mutuallyExclusive)
+                {
+                    activeButton = this;
+
+                }
+            }
+            else
+            {
+                SetButtonColor(inactiveColor);
+            }
+
+        }
+    }
+
+    private void SetButtonColor(Color color)
+    {
+
+        GetComponent<Image>().color = color;
+
+    }
+}
---
+++ Assets/Scripts/SkyboxChangeButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxChangeButton : MonoBehaviour {
+
+    public Material skybox;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeSykbox()
+    {
+
+        RenderSettings.skybox = skybox;
+        Debug.Log("IT WORKED!");
+    }
+}
